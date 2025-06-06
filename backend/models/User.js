a1a3cb1d865107c11f99feae11533ead5a6f0918
Ahmed <148172184+0x1337Ahmed@users.noreply.github.com>
2025-06-06 18:33:40 +0300

    Add backend/models/User.js 1749224080456

---
+++ backend/models/User.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide a username'],
+    unique: true,
+    trim: true,
+    minlength: [3, 'Username must be at least 3 characters long'],
+    maxlength: [30, 'Username cannot exceed 30 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+    select: false
+  },
+  profilePicture: {
+    type: String,
+    default: 'https://www.gravatar.com/avatar/?d=mp'
+  },
+  friends: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  friendRequests: {
+    sent: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    received: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  },
+  bio: {
+    type: String,
+    maxlength: [160, 'Bio cannot exceed 160 characters']
+  },
+  isOnline: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Method to get public profile (exclude sensitive data)
+userSchema.methods.toPublicProfile = function() {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.friendRequests;
+  return obj;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
